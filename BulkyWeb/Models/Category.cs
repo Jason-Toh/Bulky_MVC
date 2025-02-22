42c86c5179c47dcec38faa4e7a8e42529704fe44
Jason Toh Zhern Wee <tohjason28@gmail.com>
2025-02-22 16:23:45 +0800

    Added Category page, model and migration file

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // Type prop to create attribute
+        [Key] // Primary Key
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
